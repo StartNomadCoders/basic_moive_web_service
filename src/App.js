10897567ed6c95496346087bed54410d602366b0
Raon98 <cheol1998@kakao.com>
2023-06-13 08:45:52 +0900

    20230613 Morning Study

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import Button2 from "./Button2.js"
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setCounter((v) => v + 1);
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  const iRunOnlyOne = () => {
+    console.log('i run only one');
+  };
+  useEffect(iRunOnlyOne, []);
+  useEffect(() => {
+      if(keyword !== '' && keyword.length > 5){
+          console.log('SERACH FOR', keyword)
+      }
+  },[keyword]);
+    useEffect(() => {
+            console.log('SERACH FOR KEYWORD AND COUNTER')
+
+    },[keyword,counter]);
   return (
     <div>
-      <h1>Welcome Back!</h1>
-      <Button2 text={'Continue'}/>
+      <input
+        value={keyword}
+        text="text"
+        placeholder="Search Here"
+        onChange={onChange}
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
