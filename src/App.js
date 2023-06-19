d359b0b372aa188bbce6135e9f656593731558b5
Raon98 <cheol1998@kakao.com>
2023-06-19 19:54:43 +0900

    20230619 Night Study

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Detail from './router/Detail';
+import Home from './router/Home';
 function App() {
-  return null
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movies">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
